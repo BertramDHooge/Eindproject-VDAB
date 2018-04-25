d75b802ea498479b627c8e6a66d3e7d857b37697
Bertram <bertramdhooge@gmail.com>
2018-04-25 14:01:58 +0200

    Start met eindelijk map wat beter te implementeren

---
+++ CroCo/CroCo/MainProduce.swift
@@ -0,0 +1,13 @@
+//
+//  MainProduce.swift
+//  CroCo
+//
+//  Created by Bertram D'Hooge on 25/04/2018.
+//  Copyright © 2018 VDAB. All rights reserved.
+//
+
+import Foundation
+
+enum MainProduce: String {
+    case meat = "🍖", vegetable = "🥦", fruit = "🍏", dairy = "🥛", eggs = "🥚", poultry = "🐓"
+}
