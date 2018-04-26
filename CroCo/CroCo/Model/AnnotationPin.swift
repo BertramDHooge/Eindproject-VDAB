a798edb206189a697065357b5b2656a6c5862d55
Bertram <bertramdhooge@gmail.com>
2018-04-26 14:33:42 +0200

    MAP IS MOOIER

--- CroCo/CroCo/Model/AnnotationPin.swift
+++ CroCo/CroCo/Model/AnnotationPin.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 VDAB. All rights reserved.
 //
 
-import Foundation
+import UIKit
 import MapKit
 
 class AnnotationPin: NSObject, MKAnnotation {
@@ -26,6 +26,23 @@ class AnnotationPin: NSObject, MKAnnotation {
     
     var coordinate: CLLocationCoordinate2D {
         return producer.address
+    }
+    
+    var glyphColor: UIColor {
+        switch producer.mainProduce {
+        case .dairy:
+            return .white
+        case .eggs:
+            return .yellow
+        case .fruit:
+            return .red
+        case .meat:
+            return .brown
+        case .poultry:
+            return .brown
+        case .vegetable:
+            return .green
+        }
     }
     
     
