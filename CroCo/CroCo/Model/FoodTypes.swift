1833ba2bbe7e1d896d8d447e3ca9a0dcd2cb8f27
Louis-Hilippe Loeckx <louis.loeckx@gmail.com>
2018-05-02 14:15:30 +0200

    repositioned the producers

--- CroCo/CroCo/Model/FoodTypes.swift
+++ CroCo/CroCo/Model/FoodTypes.swift
@@ -10,19 +10,7 @@ import Foundation
 
 enum FoodTypes {
     case vegetable, meat, fruit
-    // possibly change to a structure?
-//    case vegetable: String {
-//        case carrots = "Wortel", peas = "Erwten", corn = "Mais", strawberries = "Aardbeien", tomatoes = "Tomaten", bellPeppers = "Paprika"
-//    }
-//    case meat: String {
-//        case cow = "Koe", horse = "Paard", goat = "Geit", lamb = "Lamb", pig = "Varken", deer = "Hert", boar = "Everzwijn", rabbit = "Konijn"
-//    }
-//
-//    case fruit: String {
-//        case oranges = "Sinaasappel", pears = "Peren", apples = "Appels", watermellon = "Watermeloen"
-//    }
-//
-//
+   
 }
 enum FoodName: String {
     case carrots = "Wortel", peas = "Erwten", corn = "Mais", strawberries = "Aardbeien", tomatoes = "Tomaten", bellPeppers = "Paprika"
@@ -30,8 +18,7 @@ enum FoodName: String {
     case cow = "Koe", horse = "Paard", goat = "Geit", lamb = "Lamb", pig = "Varken", deer = "Hert", boar = "Everzwijn", rabbit = "Konijn"
     
     case oranges = "Sinaasappel", pears = "Peren", apples = "Appels", watermellon = "Watermeloen"
+    
+    case cowMilk = "Cow Milk", goatMilk = "Goat Milk", horseMilk = "Horse Milk"
 }
 
-//enum Vegetable: String, FoodTypes {
-//    case carrot = "Wortel", pea = "Erwten", corn = "Mais", strawberrie = "Aardbeien", tomatoe = "Tomaten", bellPepper = "Paprika"
-//}
--- CroCo/CroCo/ViewController/ShoppingCartViewController.swift
+++ CroCo/CroCo/ViewController/ShoppingCartViewController.swift
@@ -18,19 +18,7 @@ class ShoppingCartViewController: UIViewController, UITableViewDelegate, UITable
     override func viewDidLoad() {
         super.viewDidLoad()
                 
-        let adressMammoth = Address(streetName: "Ice Lane", streetNumber: 1, postalCode: 1333, place: Place.kesselLo)
-        let mammothName = Name(firstName: "Mammoth", lastName: "Wooly")
-        let infoMammoth = Contact(name: mammothName, address: adressMammoth, telephoneNumber: "123456789", emailAddress: "imAMammoth@cold.com")
-        let mammothCrops = [Crop(cropType: FoodTypes.fruit, cropName: FoodName.apples, quantityTypes: QuantityTypes.Kg, quantity: Quantity._20, cost: 22)]
-        
-        let bertramName = Name(firstName: "Bertram", lastName: "nenHooge")
-        let adressBertram = Address(streetName: "ClosetoSchool", streetNumber: 1, postalCode: 1234, place: Place.leuven)
-        let infoBertram = Contact(name: bertramName, address: adressBertram, telephoneNumber: "0495124115", emailAddress: "veltwinkel@gmail.com")
-        let bertramCrops = [Crop(cropType: FoodTypes.meat, cropName: FoodName.cow, quantityTypes: QuantityTypes.Kg, quantity: Quantity._10, cost: 100)]
-        
-        let mammothProducer = Producer(companyName: "Tolis", contact: infoMammoth, companyImage: nil, location: CLLocationCoordinate2D(latitude: 50.98, longitude: 4.78), delivery: true, mainProduce: MainProduce.vegetableFruitDairy, deliveryHours: Date(), pickUpHours: Date(), validation: nil, crop: mammothCrops)
-        
-        let bertramProducer = Producer(companyName: "VeltWinkel", contact: infoBertram, companyImage: nil, location: CLLocationCoordinate2D(latitude: 50.98, longitude: 4.75), delivery: true, mainProduce: MainProduce.vegetableFruitEggs, deliveryHours: Date(), pickUpHours: Date(), validation: nil, crop: bertramCrops)
+
 
 
         // Do any additional setup after loading the view.
