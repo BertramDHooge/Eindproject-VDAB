47dacdf27a5bc0f24ee37c9d7221b2a35edd39aa
Ward Janssen <wardjanssen1968@gmail.com>
2018-05-07 10:03:18 +0200

    connections addProducer

---
+++ CroCo/addProducerViewController.swift
@@ -0,0 +1,66 @@
+//
+//  addProducerViewController.swift
+//  
+//
+//  Created by Ward on 07/05/2018.
+//
+
+import UIKit
+
+class addProducerViewController: UIViewController {
+
+    //    MARK: My Actions
+    
+
+    @IBAction func companyName(_ sender: UITextField) {
+    }
+    @IBAction func firstName(_ sender: UITextField) {
+    }
+    @IBAction func lastName(_ sender: UITextField) {
+    }
+    @IBAction func phoneNumber(_ sender: UITextField) {
+    }
+    @IBAction func emailAddress(_ sender: UITextField) {
+    }
+    @IBAction func delivery(_ sender: UISwitch) {
+    }
+    @IBAction func streetName(_ sender: UITextField) {
+    }
+    @IBAction func place(_ sender: UITextField) {
+    }
+    @IBAction func houseNumber(_ sender: UITextField) {
+    }
+    @IBAction func zipCode(_ sender: UITextField) {
+    }
+    @IBAction func uploadPicture(_ sender: UIButton) {
+    }
+    @IBAction func deliveryMoment(_ sender: UIDatePicker) {
+    }
+    @IBAction func pickUpMoment(_ sender: UIDatePicker) {
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
