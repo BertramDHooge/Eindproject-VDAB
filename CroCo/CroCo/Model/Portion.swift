8f894b96557299705a28944432e95b0570f05108
louis <louis.loeckx@gmail.com>
2018-05-08 17:34:09 +0200

    portion Added and further errors fixed

---
+++ CroCo/CroCo/Model/Portion.swift
@@ -0,0 +1,17 @@
+//
+//  Portion.swift
+//  CroCo
+//
+//  Created by Louis Loeckx on 08/05/2018.
+//  Copyright © 2018 VDAB. All rights reserved.
+//
+
+import Foundation
+
+struct Portion {
+    var cropType: FoodTypes
+    var cropName: FoodName
+    var quantityTypes: QuantityTypes
+    var quantity: Quantity
+    var sellingPrice: Double
+}
