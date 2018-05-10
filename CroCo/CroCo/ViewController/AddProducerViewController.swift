055c0592302225ae6d56f8181d68a1ba212b6175
Ward <wardjanssen1968@gmail.com>
2018-05-10 10:16:55 +0200

    juist gecommit zonder fout maar sourcecontrol vraagt bij checkout naar eigen branch om opnieuw te committen? So be it

--- CroCo/CroCo/ViewController/AddProducerViewController.swift
+++ CroCo/CroCo/ViewController/AddProducerViewController.swift
@@ -10,13 +10,11 @@ import UIKit
 import Firebase
 
 class AddProducerViewController: UIViewController {
-    
-//    let docRef = storage.reference
+
     
     @IBOutlet weak var companyName: UITextField!
     @IBOutlet weak var contactDotNameDotSurname: UITextField!
     @IBOutlet weak var contactDotNameDotLastname: UITextField!
-//    Maak hier eventueel een pickerView vanuit een bestaand famework met al de gemeentes
     @IBOutlet weak var place: UITextField!
     @IBOutlet weak var postalCode: UITextField!
     @IBOutlet weak var streetName: UITextField!
@@ -24,13 +22,9 @@ class AddProducerViewController: UIViewController {
     @IBOutlet weak var telephoneNumber: UITextField!
     @IBOutlet weak var emailAddress: UITextField!
     @IBOutlet weak var delivery: UISwitch!
-    
-    
-    
-    // Hier nog pickerView voor afhalingen en upload foto
+
     @IBAction func deliveryHour(_ sender: UIDatePicker) {
     }
-    
     @IBAction func pickingHour(_ sender: UIDatePicker) {
     }
     @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
@@ -49,49 +43,19 @@ class AddProducerViewController: UIViewController {
         guard let streetNumber = streetNumber.text, !streetNumber.isEmpty else {return}
         guard let telephoneNumber = telephoneNumber.text, !telephoneNumber.isEmpty else {return}
         guard let emailAddress = emailAddress.text, !emailAddress.isEmpty else {return}
-        
-        let dataToSave: [String: Any] = ["producent":["bedrijfsNaam":companyName, "voornaamContact": firstName, "achternaamContact": lastName, "woonplaats": place, "postcode": postalCode, "straatnaam": streetName, "huisnummer": streetNumber, "telefoonnummer": telephoneNumber, "emailadres": emailAddress]]
+
     }
-    
-//     let place = place.text, let postalCode = postalCode.text, let streetName = streetName.text, let streetNumber = streetNumber.text, let telephoneNumber = telephoneNumber.text, let emailAddress = emailAddress.text {
-//            let producer = Producer(companyName: companyName, contact: Contact(name: Name(firstName: firstName, lastName: lastName), address: Address(streetName: streetName, streetNumber: streetNumber, postalCode: postalCode, place: place), telephoneNumber: telephoneNumber, emailAddress: emailAddress), companyImage: <#T##String?#>, location: <#T##CLLocation#>, delivery: delivery.isOn, mainProduce: <#T##MainProduce#>, deliveryHours: <#T##Date#>, pickUpHours: <#T##Date#>, validation: <#T##Int?#>, stocks: <#T##[Stock]#>)
-//        }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
-//        if delivery.isOn {
-//            let dateFormatter = DateFormatter()
-//            dateFormatter.dateStyle = .medium
-//            return dateFormatter.string
-//        }
-//    docRef = FireStore.firestore().collection("producer").document("Producent1")
-//        docRef.setData(dataToSave) {
-//            (error) in
-//            if let error = error {
-//                print("Oh nee een fout! \(error.localizedDescription)")
-//            } else {
-//                print("Data is opgeslaan")
-//            }
-//        }
-        // Do any additional setup after loading the view.
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
     
- 
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destinationViewController.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
 
 }
