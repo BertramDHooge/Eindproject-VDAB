d2a02efb42fef12bb3da48683a365074d892c043
Bertram <bertramdhooge@gmail.com>
2018-05-14 13:32:22 +0200

    stuff

--- CroCo/CroCo/Model/AnnotationPin.swift
+++ CroCo/CroCo/Model/AnnotationPin.swift
@@ -12,9 +12,11 @@ import MapKit
 
 class AnnotationPin: NSObject, MKAnnotation {
     let producer: Producer
+    let location: CLLocationCoordinate2D
     
-    init(with producer: Producer) {
+    init(with producer: Producer, and location: CLLocationCoordinate2D) {
         self.producer = producer
+        self.location = location
     }
     
     var title: String? {
@@ -29,8 +31,9 @@ class AnnotationPin: NSObject, MKAnnotation {
     }
     
     var coordinate: CLLocationCoordinate2D {
-        return CLLocationCoordinate2D(latitude: producer.location.coordinate.latitude, longitude: producer.location.coordinate.longitude)
         
+//        return CLLocationCoordinate2D(latitude: producer.location.coordinate.latitude, longitude: producer.location.coordinate.longitude)
+        return location
     }
     
     var annotationColor: UIColor {
@@ -47,66 +50,8 @@ class AnnotationPin: NSObject, MKAnnotation {
             return .brown
         case .vegetable:
             return .green
-        case .vegetableFruit:
-            return .green
-        case .vegetabledairy:
-            return .green
-        case .vegetableEggs:
-            return .green
-        case .vegetablePoultry:
-            return .green
-        case .fruitDairy:
-            return .green
-        case .fruitEggs:
-            return .green
-        case .fruitPoultry:
-            return .green
-        case .meatVegetable:
-            return .green
-        case .meatFruit:
-            return .green
-        case .meatDairy:
-            return .green
-        case .meatEggs:
-            return .green
-        case .meatPoultry:
-            return .green
-        case .EggsPoultry:
-            return .green
-        case .meatVegetableFruit:
-            return .green
-        case .vegetableFruitEggs:
-            return .green
-        case .vegetableFruitDairy:
-            return .green
-        case .vegetableFruitPoultry:
-            return .green
-        case .meatFruitEggs:
-            return .green
-        case .meatFruitPoultry:
-            return .green
-        case .meatDairyEggs:
-            return .green
-        case .meatFruitdairy:
-            return .green
-        case .meatDairyPoultry:
-            return .green
-        case .meatVegetableFruitdairy:
-            return .green
-        case .meatVegetableFruitEggs:
-            return .green
-        case .meatVegetableFruitPoultry:
-            return .green
-        case .vegetableFruitEggsPoultry:
-            return .green
-        case .vegetableFruitdairyEggs:
-            return .green
-        case .meatDairyEggsPoultry:
-            return .green
-        case .vegetableFruitdairyEggsPoultry:
-            return .green
-        case .meatVegetableFruitdairyEggs:
-            return .green
+        default:
+            return .white
         }
     }
     
--- CroCo/CroCo/Model/Producers.swift
+++ CroCo/CroCo/Model/Producers.swift
@@ -7,15 +7,59 @@
 //
 
 import Foundation
-import CoreLocation
+import MapKit
 
 class Producers {
-    private var producers: [Producer]?
-    var deliveryLocation: CLLocation?
-    
-    private func sortProducersByDistanceToStartLocation(destinationProducerCrop: CLLocation) -> [Producer] {
-        guard let producers = producers else {return []}
-        return producers.sorted(by: {(producer1, producer2) -> Bool in (producer1).location.distance(from: self.deliveryLocation!) <                                        (producer2).location.distance(from: self.deliveryLocation!)
-        })
+    let companyName: String?
+    let contact: Contact
+    //    still find out how to stock image in FireStore
+    var companyImage: String? = nil
+    //    in order to stock a String type in FireStore
+    //    optional because when there is no delivery they might not reveal their address
+    var address: Address?
+    //    var location: CLLocationCoordinate2D
+    //    type CLLocation heeft methode distance
+    var location: CLLocation
+    var locationString: String
+    var delivery = false
+    //    make drop down list in add Producer
+    let mainProduce: MainProduce
+    //    turn Date type into string (timeintervalsince) in order to use in Firestore
+    //    this date schould specify in the date/time Picker day and starting hour of delivery and pickup
+    let deliveryHour: String?
+    let pickUpHour: String?
+    //    if there is still time to do we count the amount of validation stars commited by users
+    //    var validation: Int?
+    var stocks: [Stock]
+    //    var orderLists: [OrderList]
+    var description: String   {
+        if let companyName = companyName {
+            return "\(companyName):\n\(contact.description)\nProducten: \(mainProduce.rawValue)"
+        } else {
+            return "(contact.description)\nProducten: \(mainProduce.rawValue)"
+        }
     }
+    
+    init(companyName: String?, contact: Contact, location: CLLocation, locationString: String, delivery: Bool, mainProduce: MainProduce, deliveryHours: String, pickUpHours: String, stocks: [Stock]) {
+        self.companyName = companyName
+        self.contact = contact
+        self.location = location
+        self.locationString = locationString
+        self.delivery = delivery
+        self.mainProduce = mainProduce
+        self.deliveryHour = deliveryHours
+        self.pickUpHour = pickUpHours
+        self.stocks = stocks
+        
+    }
+    
+    //    convenience initialiser
+    
+    convenience init(contact: Contact, location: CLLocation, locationString: String, address: Address, mainProduce: MainProduce, stocks: [Stock]) {
+        self.init(companyName: nil, contact: contact, location: location, locationString: locationString, delivery: true, mainProduce: mainProduce, deliveryHours: "", pickUpHours: "", stocks: stocks)
+        
+        
+        //        self.init(companyName: nil, contact: contact, companyImage: nil, location: location, delivery: true, mainProduce: mainProduce, deliveryHours: Date(), pickUpHours: Date(), validation: nil)
+    }
+
 }
